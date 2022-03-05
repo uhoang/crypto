6fdc047f51e43b62f804074d61f1bf167f4bac67
uyen <u.hoangth@gmail.com>
2022-03-05 15:43:00 -0500

    setup script for opendax

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+useradd -g sudo -s `which bash` -m app
+echo "app ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker app
+sudo su - app
+git clone https://github.com/MobiDAX/opendax.git 
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable
+cd opendax
+source /home/app/.rvm/scripts/rvm
+rvm install "ruby-2.6.3"
+bundle install
+
